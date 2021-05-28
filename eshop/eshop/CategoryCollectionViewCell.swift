ee56a1dd6ca90afacddf07d28d03d8e93fd231f9
ikikika <iki.jito@gmail.com>
2021-05-28 22:29:20 +0800

    create category collection view cell and assign to cell

---
+++ eshop/eshop/CategoryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCollectionViewCell.swift
+//  eshop
+//
+//  Created by jit on 28/5/21.
+//  Copyright © 2021 ikikika. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}
