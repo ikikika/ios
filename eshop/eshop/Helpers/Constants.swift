a3a20d5097151474e120622895c8f936094f5674
ikikika <iki.jito@gmail.com>
2021-05-29 17:34:16 +0800

    create contants file

---
+++ eshop/eshop/Helpers/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  eshop
+//
+//  Created by jit on 29/5/21.
+//  Copyright © 2021 ikikika. All rights reserved.
+//
+
+import Foundation
+
+// Firebase headers
+public let kUSER_PATH = "User"
+public let kCATEGORY_PATH = "Category"
+public let kITEMS_PATH = "Items"
+public let kBASKET_PATH = "Basket"
+
+
+// Category
+public let kNAME = "name"
+public let kIMAGENAME = "imageName"
+public let kOBJECTID = "objectId"
