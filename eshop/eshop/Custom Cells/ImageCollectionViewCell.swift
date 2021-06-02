8492375c394218e244fab3ad9712a2401a8b9cd0
ikikika <iki.jito@gmail.com>
2021-06-02 22:20:56 +0800

    create image collection view cell, set scrolling horizontal

---
+++ eshop/eshop/Custom Cells/ImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.swift
+//  eshop
+//
+//  Created by jit on 2/6/21.
+//  Copyright © 2021 ikikika. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+}
