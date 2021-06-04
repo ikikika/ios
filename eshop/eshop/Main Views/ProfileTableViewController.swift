efbc5459337f851d73b0a148df8c69af162c0c0e
ikikika <iki.jito@gmail.com>
2021-06-04 22:44:30 +0800

    create profiletableviewcontroller cocoa touch class

---
+++ eshop/eshop/Main Views/ProfileTableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ProfileTableViewController.swift
+//  eshop
+//
+//  Created by jit on 4/6/21.
+//  Copyright © 2021 ikikika. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+   
+   
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+  
+
+}
