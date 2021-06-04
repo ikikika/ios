c2c1812b1066fc16f4d411963bfb5a10e86e55a7
ikikika <iki.jito@gmail.com>
2021-06-04 22:49:34 +0800

    connect ui with table view controller

--- eshop/eshop/Main Views/ProfileTableViewController.swift
+++ eshop/eshop/Main Views/ProfileTableViewController.swift
@@ -10,31 +10,34 @@ import UIKit
 
 class ProfileTableViewController: UITableViewController {
 
+    //MARK: - IBOutlets
+    @IBOutlet weak var finishRegistrationButtonOutlet: UIButton!
+    @IBOutlet weak var purchaseHistoryButtonOutlet: UIButton!
+    
+    
+    //MARK: - View LifeCycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        tableView.tableFooterView = UIView() // remove empty cells
     }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //check logged in status
+    }
+    
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 3
     }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        return UITableViewCell()
     }
    
    
